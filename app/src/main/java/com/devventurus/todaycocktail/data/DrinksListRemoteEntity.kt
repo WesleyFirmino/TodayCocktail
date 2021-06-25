babe9a69088dc594618a650227d150890a9ed28a
WesleyFirmino <wesleydasilva_firmino@live.com>
2021-06-24 21:35:30 -0300

    Correction using serializedName for drinks

--- app/src/main/java/com/devventurus/todaycocktail/data/DrinksListRemoteEntity.kt
+++ app/src/main/java/com/devventurus/todaycocktail/data/DrinksListRemoteEntity.kt
@@ -1,5 +1,8 @@
 package com.devventurus.todaycocktail.data
 
+import com.google.gson.annotations.SerializedName
+
 data class DrinksListRemoteEntity(
+    @SerializedName("drinks")
     val DrinkRemoteEntities: List<DrinkRemoteEntity>
 )
--- app/src/main/java/com/devventurus/todaycocktail/data/network/CocktailService.kt
+++ app/src/main/java/com/devventurus/todaycocktail/data/network/CocktailService.kt
@@ -3,7 +3,7 @@ package com.devventurus.todaycocktail.data.network
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-private const val BASE_URL = "www.thecocktaildb.com/api/json/v1/1/"
+private const val BASE_URL = "https://www.thecocktaildb.com/api/json/v1/1/"
 
 private val retrofit =
     Retrofit.Builder()
