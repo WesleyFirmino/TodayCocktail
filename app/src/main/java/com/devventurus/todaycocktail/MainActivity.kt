75aa19b5cdb6f4edb93f8cd667755c5fb0fbd5c6
WesleyFirmino <wesleydasilva_firmino@live.com>
2021-06-24 21:06:42 -0300

    Create file service Retrofit

--- app/src/main/java/com/devventurus/todaycocktail/MainActivity.kt
+++ app/src/main/java/com/devventurus/todaycocktail/MainActivity.kt
@@ -2,10 +2,31 @@ package com.devventurus.todaycocktail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
+    lateinit var drinkName: TextView
+    lateinit var drinkContainer: ConstraintLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        drinkName = findViewById(R.id.drinkName)
+        drinkContainer = findViewById(R.id.drinkContainer)
+
+        getDrink()
+
+        drinkContainer.setOnClickListener{
+            getDrink()
+        }
+    }
+
+    private fun getDrink() {
+        // Executar Requisição
+        // A partir da respota escolher um drink aleatório
+        // Colocar o nome do drink aleatório escolhido no textview
     }
 }
