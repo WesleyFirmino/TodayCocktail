d092feb86c498408778a6ab43dd03db0ec6d0f94
WesleyFirmino <wesleydasilva_firmino@live.com>
2021-06-24 21:59:58 -0300

    Request for image with glide

--- app/src/main/java/com/devventurus/todaycocktail/MainActivity.kt
+++ app/src/main/java/com/devventurus/todaycocktail/MainActivity.kt
@@ -2,10 +2,12 @@ package com.devventurus.todaycocktail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.lifecycleScope
+import com.bumptech.glide.Glide
 import com.devventurus.todaycocktail.data.DrinksListRemoteEntity
 import com.devventurus.todaycocktail.data.network.CocktailService
 import kotlinx.coroutines.Dispatchers
@@ -16,6 +18,7 @@ import java.lang.Exception
 class MainActivity : AppCompatActivity() {
     lateinit var drinkName: TextView
     lateinit var drinkContainer: ConstraintLayout
+    lateinit var drinkImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +27,9 @@ class MainActivity : AppCompatActivity() {
 
         drinkName = findViewById(R.id.drinkName)
         drinkContainer = findViewById(R.id.drinkContainer)
+        drinkImage = findViewById(R.id.drinkImage)
 
+        // Primeira imagem que inicia com o app
         getDrink()
 
         drinkContainer.setOnClickListener{
@@ -44,6 +49,7 @@ class MainActivity : AppCompatActivity() {
 
                 // Colocar o nome do drink aleatório escolhido no textview
                 drinkName.text = drink.strDrink
+                Glide.with(this@MainActivity).load(drink.strDrinkThumb).into(drinkImage)
             } catch (e: Exception) {
                 Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
             }
